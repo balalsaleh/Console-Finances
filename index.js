d3cfc8bbac885fe3270d7ef85d774b413acdbaf6
Balal Saleh <balal_saleh@outlook.com>
2023-08-22 22:19:22 +0100

    fixed code for output  as expected

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var finances = [
+let finances = [
   ['Jan-2010', 867884],
   ['Feb-2010', 984655],
   ['Mar-2010', 322013],
@@ -86,3 +86,66 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+/*
+here i have calculated the total number of months using the array.length, this returns the length which is the totalNumberOfMonths, 
+i have stored this as a const variable because it never changes from the data set we were given
+*/
+const totalNumberMonths = finances.length;
+
+/* 
+create my initial variables, I used let because they're a better practice
+for the greatestFinancialIncrease and greatestFinancialDecrease we have initilaised two properties with objects, the month and the amount
+are set to an empty string and 0. We update this inside the for loop
+*/
+let totalProfitLoss = 0;
+let totalChange = 0;
+let averageChange = 0;
+let greatestFinancialIncrease = { month: '', amount: 0 };
+let greatestFinancialDecrease = { month: '', amount: 0 };
+
+// i used a for loop to go through the finances array we have been given
+for (let indexFinances = 0; indexFinances < finances.length; indexFinances++) {
+  // get the month and the amount from the finance entry of the array
+  let [month, amount] = finances[indexFinances];
+  // add the amount value to the total running profits or losses
+  totalProfitLoss += amount;
+  // here i skipped the first entry of finances as there is nothing to compare  
+  if (indexFinances > 0) {
+    // here i have calculated the change from the previous month
+    let change = amount - finances[indexFinances - 1][1];
+    // here i have added the change to the total 
+    totalChange += change;
+    //if the change is the biggest increase so far update it
+    if (change > greatestFinancialIncrease.amount) {
+      greatestFinancialIncrease = { month, amount: change };
+    }
+    //if the change is the biggest decrease so far update it
+    if (change < greatestFinancialDecrease.amount) {
+      greatestFinancialDecrease = { month, amount: change };
+    }
+  }
+}
+
+// here i have calculated the average change, theres only 85 changes we subtract 1 from this total
+averageChange = totalChange / (totalNumberMonths - 1);
+
+/* 
+ This is what i want to print, so i used this as a guideline. 
+ Financial Analysis 
+ ----------------
+ Total Months: 86 (totalNumberMonths)
+ Total: $38382578 (totalProfitLoss)
+ Average Change: -2315.12 (averageChange.toFixed(2))
+ Greatest Increase in Profits/Losses: Feb-2012 (greatestFinancialIncrease.month) ($1926159) ((greatestFinancialIncrease.amount))
+ Greatest Decrease in Profits/Losses: Sep-2013 (greatestFinancialDecrease.month) ($-2196167) ((greatestFinancialDecrease.amount))
+*/ 
+
+//logging the financial analysis information using concatenation, i have also used toFixed(2) to give the amounto decimal places we need which is 2
+console.log(`Financial Analysis 
+----------------
+Total Months: ${totalNumberMonths}
+Total: $${totalProfitLoss}
+Average Change: ${averageChange.toFixed(2)}
+Greatest Increase in Profits/Losses: ${greatestFinancialIncrease.month} ($${greatestFinancialIncrease.amount})
+Greatest Decrease in Profits/Losses: ${greatestFinancialDecrease.month} ($${greatestFinancialDecrease.amount})`);
